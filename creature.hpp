c2aead587ef1d40c194d6fc97ae6acb683021826
kneumann00 <62099924+kneumann00@users.noreply.github.com>
2020-05-24 21:07:19 +0200

    Update creature.hpp

--- creature.hpp
+++ creature.hpp
@@ -19,9 +19,12 @@ class Creature : public Entity
 	unsigned int xp;
 	
 	Creature(std::string id, int hp, int strength,int defense, int agility,unsigned int xp);
-	
+	Creature();
 	int attack(Creature* target);
-
+	int attack_fast(Creature* target);
+	int attack_strong(Creature* target);
+	int defend();
+	int focus();
 	//losowanie statystyk wroga
 	void enemy(int room);
 };
