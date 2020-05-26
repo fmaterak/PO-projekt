4a404f81fcc6ec459ae93e2771255ae8eec9c207
kneumann00 <62099924+kneumann00@users.noreply.github.com>
2020-05-26 22:43:47 +0200

    patch_pl

--- battle.hpp
+++ battle.hpp
@@ -5,26 +5,21 @@
 
 class Creature;
 
-// Possible event types, should equate to what the player
-// can do in a battle
+// możliwe działania
 enum class BattleEventType { ATTACK, ATTACK_FAST, ATTACK_STRONG, DEFEND, FOCUS, DEFEND_ENEMY, FOCUS_ENEMY };
 
 class BattleEvent
 {
 	public:
 
-	// the attacker
 	Creature* source;
-	// the one being attacked
 	Creature* target;
-	// attack/defense
+	
 	BattleEventType type;
-
-	// Constructor
+	
 	BattleEvent(Creature* source, Creature* target, BattleEventType type);
 
-	// Convert the event type to the corresponding function and call it
-	// on the source and target
+	// rodzaj działania na funkcję
 	int run();
 };
 
@@ -32,28 +27,21 @@ class Battle
 {
 	private:
 
-	// All the creatures that are participating in the fight.
-	// We assume the player is a Creature with id "player".
-	// A vector is used because we need to get the nth element
-	// for use with a Dialogue
+	// Uczestnicy gracz z id="player" i przeciwni-k/cy
 	std::vector<Creature*> combatants;
 
-	// Remove a creature from the combatants list, and report that it's dead
+	// usunięcie pokonanych
 	void kill(Creature* creature);
 
-	// Run the next turn for the enemies and the player.
-	// Computes what the enemies should do and asks for the player's
-	// action, then compiles an event queue of the actions before
-	// proceeding through the queue and running each action.
+	// Następna tura
 	void nextTurn();
 
 	public:
 
-	// Constructor
+	// Konstruktor
 	Battle(std::vector<Creature*>& combatants);
 
-	// Run the battle until either the player dies, or all the opposing
-	// combatants do
+	// Walcz aż zostanie jeden
 	void run();
 };
 
