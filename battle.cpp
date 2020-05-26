b8804222e9a373e7c4b1a833b17179313de156ad
kneumann00 <62099924+kneumann00@users.noreply.github.com>
2020-05-26 23:20:24 +0200

    patch_pl

--- battle.cpp
+++ battle.cpp
@@ -39,18 +39,15 @@ int BattleEvent::run()
 
 void Battle::kill(Creature* creature)
 {
-	// Find the creature's position in the combatants vector
+	// Znajdz pozycje Creature w vectorze
 	auto pos = std::find(this->combatants.begin(), this->combatants.end(), creature);
 
-	// Don't try and delete the creature if it doesn't exist
+	// Nie proboj jezeli nie istnieje
 	if(pos != this->combatants.end())
 	{
-		std::cout << creature->id << " is slain!\n"; // uwaga, zmiana id moze zmienic kolejnosc działań
+		std::cout << creature->id << " zostal pokonany!\n"; // uwaga, zmiana id moze zmienic kolejnosc działań
 
-		// Health == 0 is used in main as a condition to check if the creature is
-		// dead, but this function could be called when the creature is not killed
-		// by reducing their health to zero (by a death spell, for example), so we
-		// ensure the creature's health is 0 and is marked as dead
+		// Upewnienie sie ze nie zyje :)
 		creature->hp = 0;
 		// Remove the creature
 		this->combatants.erase(pos);
@@ -64,8 +61,6 @@ void Battle::run()
 {
 	std::vector<Creature*>::iterator player;
 	std::vector<Creature*>::iterator end;
-
-	//backup postaci i przeciwnika
 
 	do
 	{
@@ -90,10 +85,10 @@ void Battle::nextTurn()
 	// kolejka, gracz pierwszy ("player">"enemy")
 	std::queue<BattleEvent> events;
 	std::sort(combatants.begin(), combatants.end(), [](Creature* a, Creature* b) { return a->id > b->id; });//jak gracz to na początek
-	// Iterate over the combatants and decide what they should do,
-	// before adding the action to the event queue.
+	// przelatywanie po uczestnikach walki
 	for(auto com : this->combatants)
 	{
+		// baardzo proste ai przciwnika
 		int enemy_action = rand() % 5 + 1;
 		switch(enemy_action)
 		{
@@ -104,7 +99,7 @@ void Battle::nextTurn()
 			}
 			case 2:
 			{
-				std::cout<<"Przeciwnik bedzie sie bronil!\n";
+				std::cout<<"Przeciwnik sie broni!\n";
 				break;
 			}
 			default:
@@ -116,25 +111,13 @@ void Battle::nextTurn()
 
 		if(com->id == "player")
 		{
-			//wybór przeciwnika
-			/*
-			// Create the target selection dialogue
-			Dialogue targetSelection = Dialogue("Who?", {});
-			// Created every turn because some combatants may die
-			for(auto target : this->combatants)
-			{
-				if(target->id != "player")
-				{
-					targetSelection.addChoice(target->id);
-				}
-			}
-			*/
+			// tu by byl wybor przeciwnika
 
-			// zapytaj o akcję
+			// zapytaj o akcje
 			std::cout<<"Co chcesz zrobic?\n1. Atak\n2. Szybki atak\n3. Silny atak\n4. Obrona\n5. Skupienie (na uniku)\n";
 			int choice;
 			std::cin>>choice;
-			//takie małe obejście dla obrony i uniku przeciwnika
+			//takie male obejscie dla obrony i uniku przeciwnika
 			if (enemy_action == 1)
 			{
 				int position = 1;
@@ -143,7 +126,6 @@ void Battle::nextTurn()
 						if(this->combatants[i]->id == "player") ++position;
 					}
 					Creature* target = this->combatants[position-1];
-					// Add the attack command to the event queue
 					events.push(BattleEvent(com, target, BattleEventType::DEFEND_ENEMY));
 			}
 			if (enemy_action == 2)
@@ -154,7 +136,6 @@ void Battle::nextTurn()
 						if(this->combatants[i]->id == "player") ++position;
 					}
 					Creature* target = this->combatants[position-1];
-					// Add the attack command to the event queue
 					events.push(BattleEvent(com, target, BattleEventType::FOCUS_ENEMY));
 			}
 
@@ -163,21 +144,16 @@ void Battle::nextTurn()
 				default:
 				case 1:
 				{
-					// Player is attacking, so ask for the target.
-					// Dialogue returns the number of the choice but with
-					// the player removed, so we have to do some fancy
-					// arithmetic to find the actual location of the target
-					// and then convert that to a pointer
-
-					// dla kilku przeciwników
-					//int position = targetSelection.activate();
+					// dla kilku przeciwników bylby tutaj wybor
+					
+					// dla 1 przeciwnika nie ma wyboru :v
 					int position = 1;
 					for(int i = 0; i < position; ++i)
 					{
 						if(this->combatants[i]->id == "player") ++position;
 					}
 					Creature* target = this->combatants[position-1];
-					// Add the attack command to the event queue
+					// dodaj do kolejki
 					events.push(BattleEvent(com, target, BattleEventType::ATTACK));
 					break;
 				}
@@ -189,7 +165,7 @@ void Battle::nextTurn()
 						if(this->combatants[i]->id == "player") ++position;
 					}
 					Creature* target = this->combatants[position-1];
-					// Add the attack command to the event queue
+					
 					events.push(BattleEvent(com, target, BattleEventType::ATTACK_FAST));
 					break;
 				}
@@ -201,19 +177,19 @@ void Battle::nextTurn()
 						if(this->combatants[i]->id == "player") ++position;
 					}
 					Creature* target = this->combatants[position-1];
-					// Add the attack command to the event queue
+					
 					events.push(BattleEvent(com, target, BattleEventType::ATTACK_STRONG));
 					break;
 				}
 				case 4:
 				{
-					// Player is defending
+					// obrona
 					events.push(BattleEvent(com, nullptr, BattleEventType::DEFEND));
 					break;
 				}
 				case 5:
 				{
-					//Player dodges
+					// skupienie sie na uniku
 					events.push(BattleEvent(com,nullptr, BattleEventType::FOCUS));
 					break;
 				}
@@ -223,7 +199,7 @@ void Battle::nextTurn()
 		{
 			if (enemy_action != 1 || enemy_action != 2)
 			{
-				// Simple enemy AI where enemy constantly attacks player
+				// atak przciwnika
 				Creature* player = *std::find_if(this->combatants.begin(), this->combatants.end(),
 					[](Creature* a) { return a->id == "player"; });
 				events.push(BattleEvent(com, player, BattleEventType::ATTACK));
@@ -231,33 +207,30 @@ void Battle::nextTurn()
 		}
 	}
 
-	// Take each event from the queue in turn and process them,
-	// displaying the results
+	// doprowadz do eventow
 	while(!events.empty())
 	{
-		// Take event from the front of the queue
+		// bierz po kolei
 		BattleEvent event = events.front();
 		switch(event.type)
 		{
 			case BattleEventType::ATTACK:
 			{
-				// The event can't be run if either the source or the
-				// target were slain previously in this turn, so we
-				// must check that they're valid first
+				// sprawdzenie czy uczestnicy moga wykonac dzialanie (czy zyja)
 				auto a = this->combatants.begin();
 				auto b = this->combatants.end();
 				if(std::find(a, b, event.source) == b || std::find(a, b, event.target) == b)
 				{
 					break;
 				}
-				//działa?
+				
 				std::cout << event.source->id
-					<< " attacks "
+					<< " bije "
 					<< event.target->id
-					<< " for "
+					<< " za "
 					<< event.run()
-					<< " damage!\n";
-				// Delete slain enemies
+					<< " obrazen!\n";
+				// zabij jesli nie zyje
 				if(event.target->hp <= 0)
 				{
 					this->kill(event.target);
@@ -272,14 +245,13 @@ void Battle::nextTurn()
 				{
 					break;
 				}
-				//działa?
 				std::cout << event.source->id
-					<< " attacks "
+					<< " bije "
 					<< event.target->id
-					<< " for "
+					<< " za "
 					<< event.run()
-					<< " damage!\n";
-				// Delete slain enemies
+					<< " obrazen!\n";
+				
 				if(event.target->hp <= 0)
 				{
 					this->kill(event.target);
@@ -294,14 +266,12 @@ void Battle::nextTurn()
 				{
 					break;
 				}
-				//działa?
 				std::cout << event.source->id
-					<< " attacks "
+					<< " bije "
 					<< event.target->id
-					<< " for "
+					<< " za "
 					<< event.run()
-					<< " damage!\n";
-				// Delete slain enemies
+					<< " obrazen!\n";
 				if(event.target->hp <= 0)
 				{
 					this->kill(event.target);
@@ -310,7 +280,7 @@ void Battle::nextTurn()
 			}
 			case BattleEventType::DEFEND:
 			{
-				std::cout << "Defense up!\n";
+				std::cout << "Obrona rosnie!\n";
 				std::cout << event.source->defense
 					<<" -> "
 					<< event.run()
@@ -319,7 +289,7 @@ void Battle::nextTurn()
 			}
 			case BattleEventType::FOCUS:
 			{
-				std::cout << "Agility up!\n";
+				std::cout << "Zrecznosc rosnie!\n";
 				std::cout << event.source->agility
 					<<" -> "
 					<< event.run()
@@ -328,7 +298,7 @@ void Battle::nextTurn()
 			}
 			case BattleEventType::DEFEND_ENEMY:
 			{
-				std::cout << "Enemy agility up!\n";
+				std::cout << "Obrona przeciwnika rosnie!\n";
 				std::cout << event.target->defense
 					<<" -> "
 					<< event.run()
@@ -337,7 +307,7 @@ void Battle::nextTurn()
 			}
 			case BattleEventType::FOCUS_ENEMY:
 			{
-				std::cout << "Enemy agility up!\n";
+				std::cout << "Zrecznosc przeciwnika rosnie!\n";
 				std::cout << event.target->agility
 					<<" -> "
 					<< event.run()
