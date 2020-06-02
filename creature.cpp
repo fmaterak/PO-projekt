cc94bf02aef187274e770aaa512b55ffc3fed573
kneumann00 <62099924+kneumann00@users.noreply.github.com>
2020-06-02 21:13:29 +0200

    *=  -->  +=

--- creature.cpp
+++ creature.cpp
@@ -52,7 +52,7 @@ int Creature::attack_fast(Creature* target)
 {
 	int damage=0;
 	//większa szansa na unik??
-	this->agility*=1.5;
+	this->agility+=1;
 	if (rand() % 100 > target->agility)
 	{
 		int attack=(this->strength)/2;
@@ -77,11 +77,11 @@ int Creature::attack_strong(Creature* target)
 {
 	int damage=0;
 	//mniejsza szansa na unik??
-	this->agility*=0.5;
+	this->agility-=1;
 	if (rand() % 100 > target->agility)
 	{
 		//większy atak
-		int attack= (this->strength)*2;
+		int attack= (this->strength) * 1.5;
 		//ignorowanie części obrony przeciwnika?
 		int defense=target->defense * 0.8;
 		if(rand() % 32 == 0)
@@ -104,14 +104,14 @@ int Creature::attack_strong(Creature* target)
 int Creature::defend()
 {
 	int defend=0;
-	this->defense*=2;
+	this->defense+=2;
 	defend= this->defense;
 	return defend;
 }
 int Creature::focus()
 {
 	int focus=0;
-	this->agility*=2;
+	this->agility+=2;
 	focus=this->agility;
 	return focus;
 }
@@ -120,14 +120,14 @@ int Creature::focus()
 int Creature::defend_enemy(Creature* target)
 {
 	int enemy_def=0;
-	target->defense*=2;
+	target->defense+=2;
 	enemy_def=target->defense;
 	return enemy_def;
 }
 int Creature::focus_enemy(Creature* target)
 {
 	int enemy_ag=0;
-	target->agility*=2;
+	target->agility+=2;
 	enemy_ag=target->agility;
 	return enemy_ag;
 }
