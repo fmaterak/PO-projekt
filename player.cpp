181f2de4069cf840634f2ad83c4a3d2783e018e2
Kamil Halamus <62609774+KamiilH@users.noreply.github.com>
2020-06-02 09:14:45 +0200

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -2,19 +2,19 @@
 #include <cmath>
 #include <fstream>
 
-#include "area.hpp"
+//#include "area.hpp"
 #include "player.hpp"
 #include "creature.hpp"
-#include "entity_manager.hpp"
-#include "weapon.hpp"
-#include "armor.hpp"
+//#include "entity_manager.hpp"
+//#include "weapon.hpp"
+//#include "armor.hpp"
 
 using namespace std;
 
-Player::Player(string id,int hp, int strength,int defense, int agility, unsigned int xp, unsigned int level):
-Creature("player",name,hp,strength,defense,agility,xp)
+Player::Player(string id, int hp, int strength, int defense, int agility, unsigned int xp, unsigned int level) :
+	Creature("player", name, hp, strength, defense, agility, xp)
 {
-	this-> level=level;
+	this->level = level;
 	this->equippedArmor = nullptr;
 	this->equippedWeapon = nullptr;
 }
@@ -28,7 +28,7 @@ Player::Player()
 	ifstream file("player.txt");
 	while (file)
 	{
-		file>>/*this->id>>*/this->hp>>this->strength>>this->defense>>this->agility>>this->xp>>this->level;
+		file >>/*this->id>>*/this->hp >> this->strength >> this->defense >> this->agility >> this->xp >> this->level;
 	}
 	file.close();
 }
@@ -36,65 +36,65 @@ Player::Player()
 //Oblicza potrzebne xp do lvlup-a --- Do dopracowania
 unsigned int Player::xpToLevel(unsigned int level)
 {
-	return (unsigned int)(1.5 * pow(this->level,3)); //TUTAJ
+	return (unsigned int)(1.5 * pow(this->level, 3)); //TUTAJ
 }
 bool Player::levelUp()
 {
 	int skill_points;
 	int choice;
 	//rozdawanie statystyk
-	if(this->xp < xpToLevel(this->level+1))
+	if (this->xp < xpToLevel(this->level + 1))
 	{
 		return false;
 	}
 	++level;
-	skill_points=5; //Ilość do rozdania
-	this->maxHp+=100; // <- Tutaj hp
-	this->hp=this->maxHp;
-	cout<<"Health grew to " << this->maxHp << "!\n";
-	cout<<"Skill points: 5\n";
-	cout<<"Choose upgrade: \n";
-	cout<<"1.Strenght [+1]\n2. Defense [+1]\n3. Agility [+1]\n";
+	skill_points = 5; //Ilość do rozdania
+	this->maxHp += 100; // <- Tutaj hp
+	this->hp = this->maxHp;
+	cout << "Health grew to " << this->maxHp << "!\n";
+	cout << "Skill points: 5\n";
+	cout << "Choose upgrade: \n";
+	cout << "1.Strenght [+1]\n2. Defense [+1]\n3. Agility [+1]\n";
 
 	//Wyeliminować echo przy stdin
-	while(skill_points)
+	while (skill_points)
 	{
-		cin>>choice;
-		switch(choice)
+		cin >> choice;
+		switch (choice)
 		{
 		case 1:
 		{
-			cout<<"Strength "<<this->strength<<" -> ";
-			this->strength+=1;
-			cout<<this->strength<<endl;
+			cout << "Strength " << this->strength << " -> ";
+			this->strength += 1;
+			cout << this->strength << endl;
 			break;
 		}
 		case 2:
 		{
-			cout<<"Defense "<<this->defense<<" -> ";
-			this->defense+=1;
-			cout<<this->defense<<endl;
+			cout << "Defense " << this->defense << " -> ";
+			this->defense += 1;
+			cout << this->defense << endl;
 			break;
 		}
 		case 3:
 		{
-			cout<<"Agility "<<this->agility<<" -> ";
-			this->agility+=1;
-			cout<<this->agility<<endl;
+			cout << "Agility " << this->agility << " -> ";
+			this->agility += 1;
+			cout << this->agility << endl;
 			break;
 		}
 		default:
 		{
-			cout<<"MissClick\n";
+			cout << "MissClick\n";
 			continue;
 		}
-		
-	}
-	skill_points--;
+
+		}
+		skill_points--;
 	}
 	return true;
 }
-
+/*
 void Player::equipWeapon(Weapon* weapon)
 {
 	this->equippedWeapon = weapon;
@@ -103,5 +103,5 @@ void Player::equipWeapon(Weapon* weapon)
 void Player::equipArmor(Armor* armor)
 {
 	this->equippedArmor = armor;
-	return;
+	return;*/
 }
